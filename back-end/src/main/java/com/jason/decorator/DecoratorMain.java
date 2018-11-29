d7989b07356519fdc29709473a4b6f8c4c4b25de
kohyusik <newtype26@naver.com>
2018-11-29 18:22:38 +0900

    design-pattern init

---
+++ back-end/src/main/java/com/jason/decorator/DecoratorMain.java
@@ -0,0 +1,6 @@
+package com.jason.decorator;
+
+public class DecoratorMain {
+	public static void main(String[] args) {
+	}
+}
---
+++ back-end/src/main/java/com/jason/factory/FactoryMain.java
@@ -0,0 +1,6 @@
+package com.jason.factory;
+
+public class FactoryMain {
+	public static void main(String[] args) {
+	}
+}
